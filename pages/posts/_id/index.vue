b39586d17e2d54581882dae560cca0552c878688
Chad Conway <agile4java@gmail.com>
2019-01-04 23:47:40 -0600

    Thru Lecture 27

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title of the Post</h1>
+      <div class="post-details">
+        <div>Last updated on XXX</div>
+        <div>Written by NAME</div>
+      </div>
+      <p>Content of the post</p>
+    </section>
+    <section class="post-feedback">Comments
+      <a href="mailto:feedback@my-email.com"></a>
+    </section>
+  </div>
+</template>
